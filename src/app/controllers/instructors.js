e241d9975aea33b9826878630f56e80f9e4688f6
jpbrab0 <jpresende502@gmail.com>
2020-11-18 23:13:35 -0300

    Refatorando estrutura de pastas.

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,10 +1,8 @@
-const { age, date } = require("../../lib/utils");
-const db = require("../../config/db");
+const Instructor = require("../models/Instructor")
 module.exports = {
   index(req,res){
-    db.query(`SELECT * FROM instructors`, (err, results) => {
-      if(err) return res.send("Database error!")
-      return res.render("instructors/index", { instructors: results.rows });
+    Instructor.all( instructors => {
+      return res.render("instructors/index", { instructors });
     })
   },
   create(req,res){
@@ -17,31 +15,9 @@ module.exports = {
         return res.send("Por favor preencha todos os dados corretamente.")
       }
     }
-    const query = `
-      INSERT INTO instructors (
-        name,
-        avatar_url,
-        gender,
-        services,
-        birth,
-        created_at
-      ) VALUES ($1, $2, $3, $4, $5, $6)
-      RETURNING id
-    `
-    const values = [
-      req.body.name,
-      req.body.avatar_url,
-      req.body.gender,
-      req.body.services,
-      date(req.body.birth).iso,
-      date(Date.now()).iso,
-    ]
-
-    db.query(query, values, (err, results) => {
-      if(err) return res.send("Database error!")
-      return
+    Instructor.create( req.body, (instructor) => {
+      return res.redirect(`/instructors/${instructor.id}`)
     })
-    return res.redirect("/")
   },
   show(req,res){
     return
