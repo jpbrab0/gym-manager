0fc1827ba0432f8dff28f1a0a94524feee9d81f6
Joao Pedro <jpresende502@gmail.com>
2021-02-22 14:43:23 -0300

    Relacionando tabelas.

--- src/app/models/Member.js
+++ src/app/models/Member.js
@@ -18,8 +18,9 @@ module.exports = {
         birth,
         blood,
         weight,
-        height
-      ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+        height,
+        instructor_id
+      ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
       RETURNING id
     `;
     const values = [
@@ -30,7 +31,8 @@ module.exports = {
       date(data.birth).iso,
       data.blood,
       data.weight,
-      data.height
+      data.height,
+      data.instructor
     ];
 
     db.query(query, values, (err, results) => {
@@ -39,7 +41,11 @@ module.exports = {
     });
   },
   find(id, callback){
-    db.query(`SELECT * FROM members WHERE id = $1`,[id], (err, results) => {
+    db.query(`
+    SELECT members.*, instructors.name AS instructor_name
+    FROM members 
+    LEFT JOIN instructors ON (members.instructor_id = instructors.id)
+    WHERE members.id = $1`,[id], (err, results) => {
       if (err) throw `Database error. ${err}`;
       callback(results.rows[0])
     })
@@ -54,8 +60,9 @@ module.exports = {
       email=($5),
       blood=($6),
       weight=($7),
-      height=($8)
-    WHERE id = $9
+      height=($8),
+      instructor_id=($9)
+    WHERE id = $10
     `
     const values = [
       data.avatar_url,
@@ -66,6 +73,7 @@ module.exports = {
       data.blood,
       data.weight,
       data.height,
+      data.instructor,
       data.id
     ]
     db.query(query, values, (err, results) => {
@@ -78,6 +86,13 @@ module.exports = {
       if (err) throw `Database error. ${err}`;
 
       return callback()
+    })
+  },
+  instructorsSelectOptions(callback){
+    db.query("SELECT name, id FROM instructors", function (err, results) {
+      if(err) throw "Database error."
+
+      callback(results.rows)
     })
   }
 };
